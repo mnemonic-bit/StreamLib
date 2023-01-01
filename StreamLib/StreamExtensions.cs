0c33b9b0d10881cd882e43de870178a451803de1
Marco Ruether <mruether@mnemonic-bit.com>
2023-01-02 00:42:49 +0100

    [master] 'fixed copy-loop between streams in ReadToEnd(); added ReadToEndAsync()'

--- StreamLib/StreamExtensions.cs
+++ StreamLib/StreamExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Threading.Tasks;
 
 namespace StreamLib
 {
@@ -20,11 +21,27 @@ namespace StreamLib
 
             using var memoryStream = new MemoryStream();
 
-            int numberOfBytesRead = 0;
-            while ((numberOfBytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
-            {
-                memoryStream.Write(buffer, 0, numberOfBytesRead);
-            }
+            stream.CopyTo(memoryStream);
+
+            return memoryStream.ToArray();
+        }
+
+        /// <summary>
+        /// Reads asynchronous the whole contents of a stream and returns
+        /// it as a byte array. This method never returns null. An exception
+        /// is thrown if the stream throws an exception during reading.
+        /// If not enough memory is available, an OutOfMemoryException
+        /// is thrown by the runtime.
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns>Returns the stream's contents as byte array.</returns>
+        public static async Task<byte[]> ReadToEndAsync(this Stream stream)
+        {
+            byte[] buffer = new byte[16 * 1024];
+
+            using var memoryStream = new MemoryStream();
+
+            await stream.CopyToAsync(memoryStream);
 
             return memoryStream.ToArray();
         }
--- StreamLibTests/StreamExtensionsTests.cs
+++ StreamLibTests/StreamExtensionsTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using StreamLib;
 using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace StreamLibTests
@@ -40,6 +41,43 @@ namespace StreamLibTests
 
             // Act
             byte[] result = stringStream.ReadToEnd();
+
+            // Assert
+            result.Should().BeEquivalentTo(textAsBytes);
+        }
+
+        [Fact]
+        public async Task ReadToEndAsync_ShouldReturnSameBytes_WhenSourceStreamContainsNoBytes_()
+        {
+            // Arrange
+            string text = "";
+            Encoding encoding = Encoding.Unicode;
+            byte[] textAsBytes = encoding.GetBytes(text);
+
+            using StringStream stringStream = new StringStream(text, encoding);
+
+            // Act
+            byte[] result = await stringStream.ReadToEndAsync();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeEmpty();
+            result.Should().BeEquivalentTo(textAsBytes);
+        }
+
+        [Theory]
+        [InlineData("SOME-TEST-TEXT")]
+        [InlineData("SOME-TEST-TEXT SOME-TEST-TEXT SOME-TEST-TEXT")]
+        public async Task ReadToEndAsync_ShouldReturnSameBytes_WhenSourceStreamContainsBytes(string text)
+        {
+            // Arrange
+            Encoding encoding = Encoding.Unicode;
+            byte[] textAsBytes = encoding.GetBytes(text);
+
+            using StringStream stringStream = new StringStream(text, encoding);
+
+            // Act
+            byte[] result = await stringStream.ReadToEndAsync();
 
             // Assert
             result.Should().BeEquivalentTo(textAsBytes);
