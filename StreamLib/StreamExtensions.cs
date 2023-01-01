b2a6992bb074d2316040d0340d218d939a36ae93
Marco Ruether <mruether@mnemonic-bit.com>
2023-01-02 00:35:48 +0100

    [master] 'added extension which reads the complete contents of a stream into a byte array'

---
+++ StreamLib/StreamExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+namespace StreamLib
+{
+    public static class StreamExtensions
+    {
+
+        /// <summary>
+        /// Reads the whole contents of a stream and returns it
+        /// as a byte array. This method never returns null. An
+        /// exception is thrown if the stream throws an exception
+        /// during reading. If not enough memory is available,
+        /// an OutOfMemoryException is thrown by the runtime.
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns>Returns the stream's contents as byte array.</returns>
+        public static byte[] ReadToEnd(this Stream stream)
+        {
+            byte[] buffer = new byte[16*1024];
+
+            using var memoryStream = new MemoryStream();
+
+            int numberOfBytesRead = 0;
+            while ((numberOfBytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                memoryStream.Write(buffer, 0, numberOfBytesRead);
+            }
+
+            return memoryStream.ToArray();
+        }
+
+    }
+}
---
+++ StreamLibTests/StreamExtensionsTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using StreamLib;
+using System.Text;
+using Xunit;
+
+namespace StreamLibTests
+{
+    public class StreamExtensionsTests
+    {
+
+        [Fact]
+        public void ReadToEnd_ShouldReturnSameBytes_WhenSourceStreamContainsNoBytes_()
+        {
+            // Arrange
+            string text = "";
+            Encoding encoding = Encoding.Unicode;
+            byte[] textAsBytes = encoding.GetBytes(text);
+
+            using StringStream stringStream = new StringStream(text, encoding);
+
+            // Act
+            byte[] result = stringStream.ReadToEnd();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeEmpty();
+            result.Should().BeEquivalentTo(textAsBytes);
+        }
+
+        [Theory]
+        [InlineData("SOME-TEST-TEXT")]
+        [InlineData("SOME-TEST-TEXT SOME-TEST-TEXT SOME-TEST-TEXT")]
+        public void ReadToEnd_ShouldReturnSameBytes_WhenSourceStreamContainsBytes(string text)
+        {
+            // Arrange
+            Encoding encoding = Encoding.Unicode;
+            byte[] textAsBytes = encoding.GetBytes(text);
+
+            using StringStream stringStream = new StringStream(text, encoding);
+
+            // Act
+            byte[] result = stringStream.ReadToEnd();
+
+            // Assert
+            result.Should().BeEquivalentTo(textAsBytes);
+        }
+
+    }
+}
